e8dc78ed2c3564c9b24c012c8528d4dc1fb438a3
unknown <suyasha091@gmail.com>
2021-03-15 13:00:28 +0530

    Added basic routes

--- app.js
+++ app.js
@@ -5,9 +5,26 @@ var express= require("express"),
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
+app.get("/", function(req, res){
     res.render("index");
 });
+app.get("/login", function(req, res){
+    res.render("login");
+});
+app.get("/register",function(req,res){
+    res.render("register");
+});
+app.get("/about",function(req,res){
+    res.render("about");
+});
+
+app.get("/gallery",function(req,res){
+    res.render("gallery");
+});
+app.get("/contact",function(req,res){
+    res.render("contact");
+});
+
 
 app.listen(3000,function(){
     console.log("Server is running");
