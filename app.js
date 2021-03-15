fe9f984824ae768853880b21f99039fa2c02ec58
unknown <suyasha091@gmail.com>
2021-03-15 13:36:31 +0530

    Added contact form

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-var express= require("express"),
-    app=express();
+var express = require("express"),
+app = express();
 
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-    res.render("index");
+app.get("/", function(req, res) {
+  res.render("index");
 });
-app.get("/login", function(req, res){
-    res.render("login");
+app.get("/login", function(req, res) {
+  res.render("login");
 });
-app.get("/register",function(req,res){
-    res.render("register");
+app.get("/register", function(req, res) {
+  res.render("register");
 });
-app.get("/about",function(req,res){
-    res.render("about");
-});
-
-app.get("/gallery",function(req,res){
-    res.render("gallery");
-});
-app.get("/contact",function(req,res){
-    res.render("contact");
+app.get("/about", function(req, res) {
+  res.render("about");
 });
 
+app.get("/gallery", function(req, res) {
+  res.render("gallery");
+});
+app.get("/contact", function(req, res) {
+  res.render("contact");
+});
 
-app.listen(3000,function(){
-    console.log("Server is running");
+
+app.listen(3000, function() {
+  console.log("Server is running");
 });
