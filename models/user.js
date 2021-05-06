ab23935bbb6330a0f7928b196afd024c985aa61b
dhruv <dhruvpatel11611@gmail.com>
2021-05-06 23:26:15 +0530

    form validation

--- models/user.js
+++ models/user.js
@@ -1,16 +1,38 @@
 var mongoose = require("mongoose");
-var passportLocalMongoose=require("passport-local-mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
     username: String,
-    email: String,
+    email: {
+        type: String,
+        validate:{
+            validator: function(v){
+                return /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(v);
+            },
+            message: '{VALUE} is not a valid email address!'
+        }
+    },
     contactno: {
         type: String,
+        validate: {
+            validator: function (v) {
+                return /^\d{10}$/.test(v);
+            },
+            message: '{VALUE} is not a valid 10 digit number!'
+        }
     },
-    dob: String,
+    dob: {
+        type: String,
+        validate: {
+            validator: function (v) {
+                return /^(0[1-9]|1[012])[-/.](0[1-9]|[12][0-9]|3[01])[-/.](19|20)\\d\\d$/.test(v);
+            },
+            message: '{VALUE} is not a valid Date!'
+        }
+    },
     password: String
 });
 
 UserSchema.plugin(passportLocalMongoose);
 
-module.exports=mongoose.model("User",UserSchema);
+module.exports = mongoose.model("User", UserSchema);
