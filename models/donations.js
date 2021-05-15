541b326ee80ad21cfb27993277ed34c166e77053
dhruv <dhruvpatel11611@gmail.com>
2021-05-15 11:17:11 +0530

    trying to display ngos

--- models/donations.js
+++ models/donations.js
@@ -3,11 +3,35 @@ var passportLocalMongoose=require("passport-local-mongoose");
 
 var NgoSchema = new mongoose.Schema({
     donor_name: String,
-    donor_email: String,
-    donor_phone: String,
+    donor_email: {
+        type: String,
+        validate:{
+            validator: function(v){
+                return /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(v);
+            },
+            message: '{VALUE} is not a valid email address!'
+        }
+    },
+    donor_phone: {
+        type: String,
+        validate: {
+            validator: function (v) {
+                return /^\d{10}$/.test(v);
+            },
+            message: '{VALUE} is not a valid 10 digit number!'
+        }
+    },
     donor_address: String,
     amount: Number,
-    donor_pan: String,
+    donor_pan: {
+        type: String,
+        validate: {
+            validator: function (v) {
+                return /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/.test(v);
+            },
+            message: '{VALUE} is not a valid Pan-number!'
+        }
+    },
     donation_user:{
         id:{
             type: mongoose.Schema.Types.ObjectId,
--- models/ngo.js
+++ models/ngo.js
@@ -3,11 +3,26 @@ var passportLocalMongoose=require("passport-local-mongoose");
 
 var NgoSchema = new mongoose.Schema({
     name: String,
-    email: String,
-    
+    email: {
+        type: String,
+        validate:{
+            validator: function(v){
+                return /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(v);
+            },
+            message: '{VALUE} is not a valid email address!'
+        }
+    },
     description: String,
     regNo: String,
-    contact: Number
+    contact: {
+        type: String,
+        validate: {
+            validator: function (v) {
+                return /^\d{10}$/.test(v);
+            },
+            message: '{VALUE} is not a valid 10 digit number!'
+        }
+    }
 });
 
 NgoSchema.plugin(passportLocalMongoose);
